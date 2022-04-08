c6305978e32a1dba80a973e92040126e3c3b71dc
gul-amd-moz <100712631+gul-amd-moz@users.noreply.github.com>
2022-04-08 11:02:22 +0200

    Chore
    Exercicio 2

---
+++ CursoOnlineDeJava/src/mz/co/explicador/Exercicio02.java
@@ -0,0 +1,45 @@
+
+package mz.co.explicador;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gulamo
+ */
+public class Exercicio02 {
+    
+    //1 faca um programa em java que receba doid numeros e exiba o resultado da sua soma.
+    
+    public static void main (String[] arg){
+        
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Insira o valor do primeiro numero: ");
+        int num1 = s.nextInt();
+        
+        System.out.println("Insira o valor do segundo numero: ");
+        int num2 = s.nextInt();
+        
+        //Soma
+        int soma = num1 + num2;
+        
+        //Subitracao
+        int sub = num1 - num2;
+        
+        //Multiplicacao
+        int mult = num1 * num2;
+        
+        //Divisao
+        double div = num1 / num2;
+         
+        
+       System.out.println("A soma de (" + num1 + ") + (" + num2 + ") = " + soma);
+       System.out.println("A subitracao de (" + num1 + ") - (" + num2 + ") = " + sub);
+       System.out.println("A Multiplicacao de (" + num1 + ") * (" + num2 + ") = " + mult);
+       System.out.println("A Divisao de (" + num1 + ") / (" + num2 + ") = " + div);
+          
+        
+    } 
+    
+}
