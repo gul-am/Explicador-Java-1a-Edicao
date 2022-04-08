59db37a29b936b19ac6e6ba822a625d517be45d6
gul-amd-moz <100712631+gul-amd-moz@users.noreply.github.com>
2022-04-08 10:54:29 +0200

    chore
    Exercicio 1

---
+++ CursoOnlineDeJava/src/mz/co/explicador/Exercicio01.java
@@ -0,0 +1,34 @@
+
+package mz.co.explicador;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gulamo
+ */
+public class Exercicio01 {
+    
+    //1 faca um programa em java que receba doid numeros e exiba o resultado da sua soma.
+    
+    public static void main (String[] arg){
+        
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Insira o valor do primeiro numero: ");
+        int num1 = s.nextInt();
+        
+        System.out.println("Insira o valor do segundo numero: ");
+        int num2 = s.nextInt();
+        
+        int soma = num1 + num2;
+        
+        System.out.println("A soma de (" + num1 + ") + (" + num2 + ") = " + soma);
+       
+        
+        
+        
+    }
+    
+    
+}
