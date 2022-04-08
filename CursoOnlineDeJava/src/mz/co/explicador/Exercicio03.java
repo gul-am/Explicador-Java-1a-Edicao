18e6118e0b856e303a0f8163c6817f11d1063a3c
gul-amd-moz <100712631+gul-amd-moz@users.noreply.github.com>
2022-04-08 11:29:30 +0200

    Chore
    Exercicio 03

---
+++ CursoOnlineDeJava/src/mz/co/explicador/Exercicio03.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mz.co.explicador;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gulamo_w10
+ */
+public class Exercicio03 {
+    
+    //3. Escreva um programa em java para determinar o cosumo medio de um automovel sendo fornrcida
+    // a distancia total pervorida pelo automovel e o total de combustivel gasto.
+    
+    public static void main (String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Insira a distancia percorida: ");
+        double distancia = s.nextDouble();
+        
+        System.out.println("Insira o combustivel gasto: ");
+        double combustivelGasto = s.nextDouble();
+        
+        double consumoMedio = distancia / combustivelGasto;
+        
+         System.out.println("Consumo medio: " + consumoMedio + " L/Km");
+        
+        
+        
+    }  
+}
