71e01906c3c70d308d6ec4af8e1e03e51f1a3c1c
gul-amd-moz <100712631+gul-am@users.noreply.github.com>
2022-05-24 17:05:51 +0200

    chore : exercicio_17i

---
+++ CursoOnlineDeJava/src/mz/co/explicador/Exercicio17i.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mz.co.explicador;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gulamo_w10
+ */
+public class Exercicio17i {
+    
+    /*
+    17. Ler 80 números e ao final informar quantos número(s) 
+    est(á)ão no intervalo entre 10 (inclusive) e 150 (inclusive).
+    */
+    
+    public static void main (String []args){
+        Scanner s = new Scanner(System.in);
+        
+        int n = 8;
+        int totalEntre = 0;
+        int[] num = new int[n];
+        
+        for (int i = 0; i < n; i++){
+            
+            System.out.print("Insira o " + (i+1) + "o numero : ");
+            num[i] = s.nextInt();
+            
+       
+     
+    }
+   
+}
+    
+}
+
